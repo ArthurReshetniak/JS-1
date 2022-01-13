90d00e8b9be44190510a15d5c20f08d87fe45657
Arthur <96789842+ArthurReshetniak@users.noreply.github.com>
2022-01-13 18:18:33 +0200

    Add files via upload

---
+++ Work-1/index.js
@@ -0,0 +1,28 @@
+const userName = "Mary";
+const userAge = "7";
+let userUndefined;
+let userNumber = null;
+let InShop = false;
+let InHome = true;
+let space = "-";
+
+console.log(userName);
+console.log(userAge);
+console.log(userNumber);
+console.log(userUndefined);
+console.log(InShop);
+console.log(InHome);
+
+console.log(space);
+console.log(space);
+
+console.log(typeof userName);
+console.log(typeof userAge);
+console.log(typeof userNumber);
+console.log(typeof userUndefined);
+console.log(typeof InShop);
+console.log(typeof InHome);
+
+console.log(space);
+
+console.log("Имя Пользователя " + userName)
